d66a0556cad702024e669c3b1da20d01fd7b1887
YingDG <zdm.1991@outlook.com>
2017-09-26 13:26:17 +0800

    spring自定义变量引入

---
+++ src/main/java/yingdg/exercise/springwebapp/config/CustomizedPropertyConfigurer.java
@@ -0,0 +1,26 @@
+package yingdg.exercise.springwebapp.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import java.util.Properties;
+
+/**
+ * Created by yingdg on 2017/9/26 0026.
+ */
+public class CustomizedPropertyConfigurer extends PropertyPlaceholderConfigurer {
+
+    @Override
+    protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props) throws BeansException {
+        super.processProperties(beanFactoryToProcess, props);
+
+        // 加载自定义静态变量
+        initStaticProps(props);
+    }
+
+    private void initStaticProps(Properties props) {
+        // TODO
+    }
+
+}
